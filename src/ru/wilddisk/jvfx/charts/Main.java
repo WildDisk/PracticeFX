ce139018e36404ada70a5fd1f2af000f43a8ce80
WildDisk <41717001+WildDisk@users.noreply.github.com>
2018-08-02 17:55:48 +1000

    Добавлены конролы для вывода 2х и 3х графиков, возвращена рамка с кнопками закрыть, свернуть, развернуть у приложения, а так же добавлены свойства disable & visible Priority для AnchorePane внутри ScrollPane.

--- src/ru/wilddisk/jvfx/charts/Main.java
+++ src/ru/wilddisk/jvfx/charts/Main.java
@@ -1,41 +1,22 @@
 package ru.wilddisk.jvfx.charts;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-
 public class Main extends Application {
-    private double xOffset;
-    private double yOffset;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("pkgGUI/mainScene.fxml"));
         primaryStage.setTitle("Hello World");
-        Scene scene = new Scene(root);
-        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = primaryStage.getX() - event.getScreenX();
-                yOffset = primaryStage.getY() - event.getScreenY();
-            }
-        });
-        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                primaryStage.setX(event.getScreenX() + xOffset);
-                primaryStage.setY(event.getScreenY() + yOffset);
-            }
-        });
-        primaryStage.setScene(scene);
-        primaryStage.setMinWidth(800);
+
+        primaryStage.setScene(new Scene(root, 880, 450));
+        primaryStage.setMinWidth(880);
         primaryStage.setMinHeight(450);
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.initStyle(StageStyle.UNIFIED);
         primaryStage.show();
     }
 
--- src/ru/wilddisk/jvfx/charts/pkgControllers/Controller.java
+++ src/ru/wilddisk/jvfx/charts/pkgControllers/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 import javafx.stage.Stage;
@@ -16,86 +17,165 @@ import java.io.StringWriter;
 
 public class Controller {
     @FXML
+    private Button btnChrt1, btnChrt2, btnChrt3;
+    @FXML
+    private AnchorPane AncPaneCh1, AncPaneCh2, AncPaneCh3;
+    @FXML
     private TextField ChartTitle, ChartLable;
     @FXML
-    private TextField inPoint1, inPoint2, inPoint3, inPoint4, inPoint5, inPoint6, inPoint7, inPoint8,
-                            inPoint9, inPoint10, inPoint11, inPoint12;
+    private TextField inCh1Point1, inCh1Point2, inCh1Point3, inCh1Point4, inCh1Point5, inCh1Point6, inCh1Point7, inCh1Point8,
+                            inCh1Point9, inCh1Point10, inCh1Point11, inCh1Point12;
+    @FXML
+    private TextField inCh2Point1, inCh2Point2, inCh2Point3, inCh2Point4, inCh2Point5, inCh2Point6, inCh2Point7, inCh2Point8,
+                            inCh2Point9, inCh2Point10, inCh2Point11, inCh2Point12;
+    @FXML
+    private TextField inCh3Point1, inCh3Point2, inCh3Point3, inCh3Point4, inCh3Point5, inCh3Point6, inCh3Point7, inCh3Point8,
+                            inCh3Point9, inCh3Point10, inCh3Point11, inCh3Point12;
+
+//      Поля для расчёта ResizeWindows
+//=======================================
+//    @FXML
+//    public Button btnResize;
+//
+//    private double dx, dy;
+//=======================================
 
     public void showChart(ActionEvent actionEvent) {
-        try {
-            Stage stage = new Stage();
-            stage.setTitle("Chart");
-            final NumberAxis xAxis = new NumberAxis();
-            final NumberAxis yAxis = new NumberAxis();
-            xAxis.setLabel(ChartLable.getText());
-            //creating the chart
-            final LineChart<Number,Number> lineChart =
-                    new LineChart<Number,Number>(xAxis,yAxis);
+        if (AncPaneCh1.disableProperty().getValue().equals(false)) {
+            try {
+                Stage stage = new Stage();
+                stage.setTitle("Chart");
+                final NumberAxis xAxis = new NumberAxis();
+                final NumberAxis yAxis = new NumberAxis();
+                xAxis.setLabel(ChartLable.getText());
+                //creating the chart
+                final LineChart<Number, Number> lineChart =
+                        new LineChart<Number, Number>(xAxis, yAxis);
 
-            lineChart.setTitle(ChartTitle.getText());
-            //defining a series
-            XYChart.Series series = new XYChart.Series();
-            series.setName("My portfolio");
-            //populating the series with data
-            series.getData().add(new XYChart.Data(1, Integer.parseInt(inPoint1.getText())));
-            series.getData().add(new XYChart.Data(2, Integer.parseInt(inPoint2.getText())));
-            series.getData().add(new XYChart.Data(3, Integer.parseInt(inPoint3.getText())));
-            series.getData().add(new XYChart.Data(4, Integer.parseInt(inPoint4.getText())));
-            series.getData().add(new XYChart.Data(5, Integer.parseInt(inPoint5.getText())));
-            series.getData().add(new XYChart.Data(6, Integer.parseInt(inPoint6.getText())));
-            series.getData().add(new XYChart.Data(7, Integer.parseInt(inPoint7.getText())));
-            series.getData().add(new XYChart.Data(8, Integer.parseInt(inPoint8.getText())));
-            series.getData().add(new XYChart.Data(9, Integer.parseInt(inPoint9.getText())));
-            series.getData().add(new XYChart.Data(10, Integer.parseInt(inPoint10.getText())));
-            series.getData().add(new XYChart.Data(11, Integer.parseInt(inPoint11.getText())));
-            series.getData().add(new XYChart.Data(12, Integer.parseInt(inPoint12.getText())));
+                lineChart.setTitle(ChartTitle.getText());
+                //defining a series
+                XYChart.Series series = new XYChart.Series();
+                series.setName("My portfolio");
+                //populating the series with data
+                series.getData().add(new XYChart.Data(1, Integer.parseInt(inCh1Point1.getText())));
+                series.getData().add(new XYChart.Data(2, Integer.parseInt(inCh1Point2.getText())));
+                series.getData().add(new XYChart.Data(3, Integer.parseInt(inCh1Point3.getText())));
+                series.getData().add(new XYChart.Data(4, Integer.parseInt(inCh1Point4.getText())));
+                series.getData().add(new XYChart.Data(5, Integer.parseInt(inCh1Point5.getText())));
+                series.getData().add(new XYChart.Data(6, Integer.parseInt(inCh1Point6.getText())));
+                series.getData().add(new XYChart.Data(7, Integer.parseInt(inCh1Point7.getText())));
+                series.getData().add(new XYChart.Data(8, Integer.parseInt(inCh1Point8.getText())));
+                series.getData().add(new XYChart.Data(9, Integer.parseInt(inCh1Point9.getText())));
+                series.getData().add(new XYChart.Data(10, Integer.parseInt(inCh1Point10.getText())));
+                series.getData().add(new XYChart.Data(11, Integer.parseInt(inCh1Point11.getText())));
+                series.getData().add(new XYChart.Data(12, Integer.parseInt(inCh1Point12.getText())));
 
-            Scene scene  = new Scene(lineChart,800,600);
-            scene.getStylesheets().add("ru/wilddisk/jvfx/charts/pkgCSS/chartStyle.css");
-            lineChart.getData().add(series);
+                Scene scene = new Scene(lineChart, 800, 600);
+                scene.getStylesheets().add("ru/wilddisk/jvfx/charts/pkgCSS/chartStyle.css");
+                lineChart.getData().add(series);
 
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Alarm!");
-            alert.setHeaderText("Ой! Видно что то пошло не так!");
-            alert.setContentText("По любому пытался буквы ввести, да?\n" +
-                                 "Невозможно построить график используя буквы!\n" +
-                                 "Как ты вообще себе это представляешь?");
+                stage.setScene(scene);
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Alarm!");
+                alert.setHeaderText("Ой! Видно что то пошло не так!");
+                alert.setContentText("По любому пытался буквы ввести, да?\n" +
+                        "Невозможно построить график используя буквы!\n" +
+                        "Как ты вообще себе это представляешь?");
 
-            Exception ex = new Exception("StackTrace");
+                Exception ex = new Exception("StackTrace");
 
 // Create expandable Exception.
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            ex.printStackTrace(pw);
-            String exceptionText = sw.toString();
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                ex.printStackTrace(pw);
+                String exceptionText = sw.toString();
 
-            Label label = new Label("The exception stacktrace was:");
+                Label label = new Label("The exception stacktrace was:");
 
-            TextArea textArea = new TextArea(exceptionText);
-            textArea.setEditable(false);
-            textArea.setWrapText(true);
+                TextArea textArea = new TextArea(exceptionText);
+                textArea.setEditable(false);
+                textArea.setWrapText(true);
 
-            textArea.setMaxWidth(Double.MAX_VALUE);
-            textArea.setMaxHeight(Double.MAX_VALUE);
-            GridPane.setVgrow(textArea, Priority.ALWAYS);
-            GridPane.setHgrow(textArea, Priority.ALWAYS);
+                textArea.setMaxWidth(Double.MAX_VALUE);
+                textArea.setMaxHeight(Double.MAX_VALUE);
+                GridPane.setVgrow(textArea, Priority.ALWAYS);
+                GridPane.setHgrow(textArea, Priority.ALWAYS);
 
-            GridPane expContent = new GridPane();
-            expContent.setMaxWidth(Double.MAX_VALUE);
-            expContent.add(label, 0, 0);
-            expContent.add(textArea, 0, 1);
+                GridPane expContent = new GridPane();
+                expContent.setMaxWidth(Double.MAX_VALUE);
+                expContent.add(label, 0, 0);
+                expContent.add(textArea, 0, 1);
 
-            // Set expandable Exception into the dialog pane.
-            alert.getDialogPane().setExpandableContent(expContent);
-            alert.showAndWait();
+                // Set expandable Exception into the dialog pane.
+                alert.getDialogPane().setExpandableContent(expContent);
+                alert.showAndWait();
+            }
+        } else {
+            System.out.println("Куй!");
         }
     }
-    @FXML
-    public void closeDialog(ActionEvent actionEvent) {
-        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
+
+//=========================================================================================
+//              Попытка сделать ResizeWindow
+///////////////////////////////////////////////////////////////////////////////////////////
+//    @FXML
+//    public void closeDialog(ActionEvent actionEvent) {
+//        ((Stage)(((Button)actionEvent.getSource()).getScene().getWindow())).close();
+//    }
+//
+//    @FXML
+//    public void resizeCornerPress(MouseEvent mouseEvent) {
+//        Stage stage;
+//        stage = (Stage)btnResize.getScene().getWindow();
+//        dx = mouseEvent.getScreenX() - stage.getX();
+//        dy = mouseEvent.getScreenY() - stage.getY();
+//    }
+//
+//    @FXML
+//    public void resizeCornerDragg(MouseEvent mouseEvent) {
+//        Stage stage;
+//        stage = (Stage)btnResize.getScene().getWindow();
+//        stage.setWidth(mouseEvent.getScreenX() + stage.getX());
+//        stage.setHeight(mouseEvent.getScreenY() + stage.getY());
+//    }
+////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void showMenuOneChart(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnChrt1) {
+            AncPaneCh1.toFront();
+            AncPaneCh1.disableProperty().setValue(false);
+            AncPaneCh1.visibleProperty().setValue(true);
+            AncPaneCh2.disableProperty().setValue(true);
+            AncPaneCh2.visibleProperty().setValue(true);
+            AncPaneCh3.disableProperty().setValue(true);
+            AncPaneCh3.visibleProperty().setValue(true);
+        }
+    }
+
+    public void showMenuTwoChart(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnChrt2) {
+            AncPaneCh2.toFront();
+            AncPaneCh1.disableProperty().setValue(true);
+            AncPaneCh1.visibleProperty().setValue(true);
+            AncPaneCh2.disableProperty().setValue(false);
+            AncPaneCh2.visibleProperty().setValue(true);
+            AncPaneCh3.disableProperty().setValue(true);
+            AncPaneCh3.visibleProperty().setValue(true);
+        }
+    }
+
+    public void showMenuThreeChart(ActionEvent actionEvent) {
+        if (actionEvent.getSource() == btnChrt3) {
+            AncPaneCh3.toFront();
+            AncPaneCh1.disableProperty().setValue(true);
+            AncPaneCh1.visibleProperty().setValue(true);
+            AncPaneCh2.disableProperty().setValue(true);
+            AncPaneCh2.visibleProperty().setValue(true);
+            AncPaneCh3.disableProperty().setValue(false);
+            AncPaneCh3.visibleProperty().setValue(true);
+        }
     }
 }
